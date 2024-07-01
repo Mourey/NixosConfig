3a7e3c4b74575a69c1d116524ff7897fff856622
Rodrigo Mourey <rodmourey@proton.me>
2024-07-01 13:04:35 +0200

    making changes

--- nixosModules/system/overlay.nix
+++ nixosModules/system/overlay.nix
@@ -1,14 +1,24 @@
-{ inputs }:
+{ inputs, pkgs, ... }:
 final: prev:
 let
   inherit (final)
     callPackage
     ;
+  toolchain = (pkgs.rustChannelOf {
+    rustToolchain = ../rust-toolchain.toml;
+    sha256 = "";
+    # After you run `nix build`, replace this with the actual
+    # hash from the error message
+  }).rust;
+
 in
 {
   # misc
   wezterm = callPackage ./wezterm {
     wezterm-flake = inputs.wezterm;
-    naersk = callPackage inputs.naersk { };
+    naersk = callPackage inputs.naersk {
+      cargo = toolchain;
+      rustc = toolchain;
+    };
   };
 }
